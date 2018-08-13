f86ebf2aa496f657fa124d3bc12428a0de5b3093
manasareddybethi <manasa.r.bethi@centene.com>
2018-08-13 10:52:32 -0500

    commit-check

--- hellojenkins.go
+++ hellojenkins.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello Jenkins!")
 	fmt.Println("hey")
-	fmt.Println("Check to see if build is triggered")
+	fmt.Println("Check to see if build triggered!!")
 }
